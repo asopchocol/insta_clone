a06b64ae118f2152a8e2de1748dac5bc193ccd4c
asopchocol <asopchocol@gmail.com>
2022-03-19 15:43:37 +0900

    project 생성

---
+++ src/test/java/hero/insta_clone/InstaCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package hero.insta_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
