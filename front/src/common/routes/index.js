752ffacae68e6c93e21f9765f7db3daedb8d47bd
wjs487 <wjs487@naver.com>
2022-03-27 19:20:40 +0900

    프론트임시작업

--- front/src/common/routes/index.js
+++ front/src/common/routes/index.js
@@ -1,9 +1,11 @@
-import Home from '../../pages/Home';
-import LoginPage from '../../pages/users/Login';
+import Home from "../../pages/Home";
+import LoginPage from "../../pages/users/Login";
+import SignupPage from "../../pages/users/Signup";
 
 const routes = [
-  { path: '/', components: Home },
-  { path: '/login', components: LoginPage },
+  { path: "/", components: Home },
+  { path: "/login", components: LoginPage },
+  { path: "/signup", components: SignupPage },
 ];
 
 export default routes;
