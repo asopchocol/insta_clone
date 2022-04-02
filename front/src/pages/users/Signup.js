dc4fd4bd682a735f50f49313e05869aaccff98c4
insomenia-jehoon <yjhoon0816@insomenia.com>
2022-04-02 19:29:47 +0900

    api 일부 작업

--- front/src/pages/users/Signup.js
+++ front/src/pages/users/Signup.js
@@ -1,14 +1,45 @@
 import { Formik } from "formik";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as Yup from "yup";
+import axios from "axios";
 
 const SignupPage = () => {
+  useEffect(() => {
+    // http method | "restful API"
+    // ------------|----------------
+    // GET         | /users/:id
+    // POST        | /users
+    // PATCH, PUT  | /users/:id
+    // DELETE      | /users/:id
+    // axios
+    //   .post({
+    //     method: "POST",
+    //     url: "http://127.0.0.1:8080/users/signup",
+    //     data: {
+    //       email: "111@naver.com",
+    //       passwd: "111111",
+    //     },
+    //   })
+    //   .then((res) => {
+    //     console.log(res);
+    //   });
+    // axios
+    //   .post("/users/signup", {
+    //     email: "111@naver.com",
+    //     passwd: "111111",
+    //   })
+    //   .then((res) => {
+    //     console.log(res);
+    //   });
+  }, []);
+
   const [passwordType, setPasswordType] = useState("password");
+
   const loginSchema = Yup.object().shape({
     email: Yup.string().required("필수 입력사항입니다."),
     name: Yup.string().required("필수 입력사항입니다."),
-    id: Yup.string().required("필수 입력사항입니다."),
-    password: Yup.string().required("필수 입력사항입니다."),
+    nickname: Yup.string().required("필수 입력사항입니다."),
+    passwd: Yup.string().required("필수 입력사항입니다."),
   });
 
   return (
@@ -22,21 +53,17 @@ const SignupPage = () => {
           <h1 className="text-center text-gray-400 font-semibold">
             가입하세요.
           </h1>
-          <div className="mx-5 mb-2 mt-5">
-            <button className="bg-blue-500 text-white w-full py-1">
-              Facebook으로 로그인
-            </button>
-          </div>
-          <div className="text-center font-normal text-sm  mb-3 mt-3">또는</div>
 
           <Formik
-            initialValues={{ email: "", password: "" }}
+            initialValues={{ email: "", name: "", nickname: "", passwd: "" }}
             validationSchema={loginSchema}
             onSubmit={(values, { setSubmitting }) => {
-              setTimeout(() => {
-                alert(JSON.stringify(values, null, 2));
-                setSubmitting(false);
-              }, 400);
+              debugger;
+              console.log(values);
+              setSubmitting(false);
+              // setTimeout(() => {
+              //   alert(JSON.stringify(values, null, 2));
+              // }, 400);
             }}
             enableReinitialize
             validateOnMount
@@ -48,7 +75,8 @@ const SignupPage = () => {
                     className="border border-gray-300 text-sm bg-gray-50 w-full p-1"
                     onChange={handleChange}
                     onBlur={handleBlur}
-                    type="email"
+                    type="text"
+                    name="email"
                     placeholder="휴대폰 번호 또는 이메일주소"
                   />
                 </div>
@@ -57,7 +85,8 @@ const SignupPage = () => {
                     className="border border-gray-300 bg-gray-50 w-full p-1"
                     onChange={handleChange}
                     onBlur={handleBlur}
-                    type="name"
+                    type="text"
+                    name="name"
                     placeholder="성명"
                   />
                 </div>
@@ -66,7 +95,8 @@ const SignupPage = () => {
                     className="border border-gray-300 bg-gray-50 w-full p-1"
                     onChange={handleChange}
                     onBlur={handleBlur}
-                    type="id"
+                    type="text"
+                    name="nickname"
                     placeholder="사용자 이름"
                   />
                 </div>
@@ -76,38 +106,22 @@ const SignupPage = () => {
                     onChange={handleChange}
                     onBlur={handleBlur}
                     type={passwordType}
+                    name="passwd"
                     placeholder="비밀번호"
                   />
                 </div>
                 <div className="mx-5 mb-2 mt-5">
                   <button
+                    type="submit"
                     disabled={!isValid || isSubmitting}
                     className="bg-sky-300 text-white w-full py-1"
                   >
-                    로그인
+                    회원가입
                   </button>
-                </div>
-
-                <div className="text-center font-medium mb-2 mt-5 ">
-                  <a href="#">Facebook으로 로그인</a>
-                </div>
-                <div className="text-center text-xs font- thin mb-6 mt-5">
-                  <a href="#">비밀번호를 잊으셨나요?</a>
                 </div>
               </form>
             )}
           </Formik>
-        </div>
-      </article>
-
-      <article className="border border-gray-300">
-        <div className="w-96 m-auto bg-white">
-          <div className="text-center font-medium mb-5 mt-5 ">
-            계정이 없으신가요?
-            <a className="text-blue-500" href="/signup">
-              가입하기
-            </a>
-          </div>
         </div>
       </article>
     </main>
